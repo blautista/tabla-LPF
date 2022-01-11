5580ced76eabe66804cc63d3f21f490e3d7ca6fd
blautista <bautistaromanazzi@gmail.com>
2022-01-10 23:58:43 -0300

    finished scraping and front-end data fetching

--- server/index.js
+++ server/index.js
@@ -1,26 +1,48 @@
-const cheerio = require('cheerio')
-const express = require('express')
-const request = require('request')
+const cheerio = require("cheerio");
+const express = require("express");
+const axios = require("axios");
+const request = require("request");
 
 const app = express();
 
-const getSiteData = () => {
-  try {
-    request("https://www.futbolargentino.com/primera-division/tabla-de-posiciones", (err, res, body) => {
-      const $ = cheerio.load(body);
+//pos equipo pj g e p gf gc dg pts
 
-      const table = $('tbody');
-      table.children('tr').each((itr, tr) => {
-        $(tr).children().each((itd, td) => {
-          console.log($(td).text().replace(/\s\s+/g, ''));
+const getSiteData = async () => {
+  try {
+    const res = await axios.get(
+      "https://www.futbolargentino.com/primera-division/tabla-de-posiciones"
+    );
+    let dataArray = [];
+    const $ = cheerio.load(res.data);
+
+    const table = $("tbody");
+    table.children("tr").each((itr, tr) => {
+      let rowArray = [];
+      $(tr)
+        .children()
+        .each((itd, td) => {
+          rowArray.push($(td).text().replace(/\s\s+/g, ""));
+          // console.log($(td).text().replace(/\s\s+/g, ''));
         });
-      });
+      dataArray.push(rowArray);
     });
+    
+    return dataArray;
   } catch (error) {
     console.log(error);
+    return error;
   }
-  
-  
-}
+};
 
-getSiteData();
+app.get("/posiciones", async (req, res) => {
+  const data = await getSiteData();
+  console.log(data);
+  // res.send(JSON.stringify(data));
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Content-Type', 'application/json');
+  res.status(200).send(JSON.stringify(data));
+});
+
+app.listen(3000, () => {
+  console.log("listening...");
+});
