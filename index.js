2ce6d787db0fa63639575adbd27c60563a0e19ac
blautista <bautistaromanazzi@gmail.com>
2022-01-11 23:39:09 -0300

    added mongodb connections and job scheduler

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const cheerio = require("cheerio");
+const schedule = require("node-schedule");
 const express = require("express");
-const { MongoClient } = require('mongodb');
-require('dotenv').config();
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
 const axios = require("axios");
 
 const hbs = require("express-handlebars");
@@ -18,35 +19,59 @@ app.engine(
 app.set("views", __dirname + "/views");
 app.set("view engine", "hbs");
 //pos equipo pj g e p gf gc dg pts
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", async (req, res) => {
-  const data = await getSiteData();
-  res.render("index", { tableData: data });
-  console.log('rendered');
+  const data = await getLatestDatabaseData();
+  const standings = data.standings;
+  console.log(data);
+  res.render("index", { tableData: standings });
+  console.log("rendered");
 });
 
 app.listen(process.env.PORT, () => {
   console.log("listening...");
 });
 
+const scrapingJob = schedule.scheduleJob("*/1 * * * *", async () => {
+  console.log("Trying to scrape....");
+  try {
+    const res = await saveSiteDataOnDatabase();
+    console.log("Successfully fetched data");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 const getLatestDatabaseData = async () => {
-  const client = await MongoClient.connect(`mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.gpmz6.mongodb.net/standings?retryWrites=true&w=majority`);
+  const client = await MongoClient.connect(
+    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.gpmz6.mongodb.net/standings?retryWrites=true&w=majority`
+  );
   const db = client.db();
-  const standingsCollection = db.collection('standings');
-  const data = standingsCollection.find().limit(1).sort({$natural:-1})
+  const standingsCollection = db.collection("standings");
+  const data = await standingsCollection.find().limit(1).sort({ $natural: -1 }).toArray();
+  console.log(data[0]);
   client.close();
-  return data;
-}
+  return data[0];
+};
 
 const saveSiteDataOnDatabase = async () => {
-  const data = await getSiteData();
-  const client = await MongoClient.connect(`mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.gpmz6.mongodb.net/standings?retryWrites=true&w=majority`);
-  const db = client.db();
-  const standingsCollection = db.collection('standings');
-  client.close();
-  standingsCollection.insertOne(data);
-}
+  try {
+    const data = await getSiteData();
+    const client = await MongoClient.connect(
+      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.gpmz6.mongodb.net/standings?retryWrites=true&w=majority`
+    );
+    const db = client.db();
+    const standingsCollection = db.collection("standings");
+    const res = await standingsCollection.insertOne({ standings: data });
+    client.close();
+    console.log("Data saved succesfully");
+    return 1;
+  } catch (error) {
+    console.log(error);
+    return 0;
+  }
+};
 
 const getSiteData = async () => {
   try {
